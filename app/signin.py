dbefef873bfb9853f408546a2bc93d65ba7b84f4
Abhay-Pillai <abhaypillai26@gmail.com>
2025-10-06 22:48:05 +0530

    Update signin.py

--- app/signin.py
+++ app/signin.py
@@ -37,13 +37,12 @@ def test_page():
 def get_db_connection():
     connection = mysql.connector.connect(
         host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", 3306)),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASS"),
         database=os.getenv("DB_NAME")
     )
     return connection
-
-
 
 @app.route("/")
 def animation():
@@ -1397,4 +1396,5 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
